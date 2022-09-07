f192b50a5e34b0e84ccd178b5c3a575062e7b45a
diegojimenez77 <gjpcoding8652@gmail.com>
2022-09-07 18:06:57 -0500

    adding generators example

--- main.js
+++ main.js
@@ -13,4 +13,41 @@ const helloPromise = () => {
        .then( response => console.log(response))
        .then(() => console.log('hola'))
        .catch(error => console.log(error));
-   
+
+// Clases
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+import {hello} from './module';
+
+hello();
+
+//Generators
+function* helloWorld() {
+    if (true) {
+        yield 'Hello, ';
+    }
+    if (true) {
+        yield 'World';
+    }
+    if (true) {
+        yield 'Webos!';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
