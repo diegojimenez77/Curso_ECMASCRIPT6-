277c462b7ac763545f0943ba92301ac3e433920f
diegojimenez77 <gjpcoding8652@gmail.com>
2022-09-07 19:30:27 -0500

    adding es9

--- es8/index.js
+++ es8/index.js
@@ -23,3 +23,32 @@ const string = 'hello';
 console.log(string.padStart(20,'hi'));
 console.log(string.padEnd(20, '-'));
 console.log('food'.padEnd(20, '='));
+
+//async await
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    });
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+//other example
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error){
+        console.log(error);
+    }
+};
+
+anotherFunction();
